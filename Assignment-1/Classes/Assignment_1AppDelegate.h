345b75e561c3e2d11569d2ffbda500ec7c0c8bf1
Christopher McCulloh <cmcculloh@Christopher-McCullohs-MacBook-Pro.local>
2010-09-02 20:40:58 -0400

    created assignment 1 project with text to be changed

---
+++ Assignment-1/Classes/Assignment_1AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  Assignment_1AppDelegate.h
+//  Assignment-1
+//
+//  Created by Christopher McCulloh on 9/2/10.
+//  Copyright The Finish Line, Inc. 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface Assignment_1AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ Assignment-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment-1
+//
+//  Created by Christopher McCulloh on 9/2/10.
+//  Copyright The Finish Line, Inc. 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"Assignment_1AppDelegate");
+    [pool release];
+    return retVal;
+}
